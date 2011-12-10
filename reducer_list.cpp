942ea21062394ee5f5f590f372425547fd1ebbb4
Boon Teik Ooi <ooiboonteik@gmail.com>
2011-12-10 12:48:11 -0500

    list reducer

--- reducer_list.cpp
+++ reducer_list.cpp
@@ -1,5 +1,5 @@
-#ifndef PARALLEL_SOLVER
-#define PARALLEL_SOLVER
+#ifndef REDUCER_LIST
+#define REDUCER_LIST
 
 #include <iostream>
 #include <cassert>
@@ -7,128 +7,109 @@
 #include <cilk/cilk.h>
 #include <cilk/reducer_opadd.h>
 #include <cilk/reducer_min.h>
+#include <cilk/reducer_list.h>
 #include <pthread.h>
 
 #define print(x) std::cout << x << std::endl
 
-class parallel_solver
+class reducer_list
 {
 private:
 	graph *g;
 	vector<vertex*>* vertices;
-	cilk::reducer_opadd<int> *excess_tmp;
-	vector<int> ex;
 	vector<pthread_mutex_t> vlocks;
-	cilk::reducer_opadd<int> active;
-//	int active;
-	pthread_mutex_t alock;
+	vector<int> ex;
+	list<int> active;
 
 public:
-	parallel_solver(graph *G) : g(G), vertices(G->v()) 
-	{
+	reducer_list(graph *G) : g(G), vertices(G->v()) {
 		vlocks.resize(g->n());
-		ex.resize(g->n(), 0);
-		excess_tmp = new cilk::reducer_opadd<int>[g->n()];
+		ex.resize(g->n());
 		g->s()->set_height(g->n());
-//		active = 0;
-		pthread_mutex_init(&alock, NULL);
 		for (int i = 0; i < g->n(); ++i) {
 			pthread_mutex_init(&vlocks[i], NULL);
 		}
 	}
 
-	~parallel_solver() { delete[] excess_tmp; }
+	~reducer_list() {
+		for (int i = 0; i < g->n(); ++i)
+			pthread_mutex_destroy(&vlocks[i]);
+	}
 	int solve_maxflow();
-	int pulse(int);
-	void push(int);
+	bool pulse(int);
+	bool push(int);
 	void relabel(int);
 };
 
-int parallel_solver::solve_maxflow()
+int reducer_list::solve_maxflow()
 {
 	vertex *s = g->s();
 	vector<edge*>* s_adj = s->edges();
-	edge *e;
 	int a = 0;
+	cilk::reducer_list_append<int> active_v;
 	for (int i = 0; i < s_adj->size(); ++i) {
-		e = s_adj->at(i);
+		edge *e = s_adj->at(i);
 		e->update_flow(s, e->upper(s));
 		e->opposite(s)->update_excess(e->upper(s));
-		if (e->opposite(s)->excess() > 0)
-			++a;
+		active.push_back(e->opposite(s)->index());
 	}
-//	cilk_for (int i = 0; i < g->n(); ++i) {
-//		if (excess_tmp[i].get_value() != 0)
-//			print("initialize: excess reducer not 0");
-//	}
+
+//	g->display_flow();
+//	active = active_v.get_value();
+//	print("initial size: " << active.size());
 	int counter = 0;
-	while (a > 0) {
-		a = pulse(counter);
+	while	(pulse(counter)) {
 //		g->display_flow();
-//		print("count: " << counter << ", active: " << a);
-		counter++;
+//		print("count: " << counter << ", active: " << active.size());
+//		counter++;
 	}
 	return 0;
 }
 
-int parallel_solver::pulse(int c)
+//return whether there still active nodes
+bool reducer_list::pulse(int c)
 {
-//	cilk::reducer_opadd<int> active;
-	int active = 0;
-	vector<edge*>* edges;
-//	cilk_for (int i = 2; i < vertices->size(); ++i) {
-//		int e = excess_tmp[i].get_value();
-//		assert(ex[i] == 0);
-//		if (e != 0)
-//			print("excess for " << i << " not cleaned before push");
-//	}
-	cilk_for (int i = 2; i < vertices->size(); ++i) {
-		push(i);
+	cilk::reducer_list_append<int> rl;
+	list<int>::iterator it = active.begin();
+	for (; it != active.end(); ++it) {
+		if (push(*it)) {
+			rl.push_back(*it);
+		}
 	}			
 	cilk_sync;
-	cilk_for(int i = 2; i < vertices->size(); ++i) {
+	list<int> excess_v = rl.get_value();
+	for (it = excess_v.begin(); it != excess_v.end(); ++it) {
+		relabel(*it);
+	}
+
+	cilk::reducer_list_append<int> active_r;
+	cilk_for (int i = 2; i < vertices->size(); ++i) {
 		vertex *v = vertices->at(i);
-		if (v->excess() > 0) {
-			relabel(i);
-		}
-		int exc = excess_tmp[i].get_value();
-		exc = ex[i];
-		assert(exc >= 0);
-//		if (exc != ex[i])
-//			print("count: " << c << " v " << i << " exc: " << exc << " ex: " << ex[i]);
-		v->update_excess(exc);
+		v->update_excess(ex[i]);
 		ex[i] = 0;
-		excess_tmp[i] -= exc;
-//		if (excess_tmp[i].get_value() != 0)
-//			print(i << "excess not cleaned: " << excess_tmp[i].get_value());
-//		excess_tmp[i].get_value();
-		assert(excess_tmp[i].get_value() == 0);
 		if (v->excess() > 0) {
-			pthread_mutex_lock(&alock);
-//			print(i << " is active with excess " << v->excess());
-			active += 1;
-			pthread_mutex_unlock(&alock);
+			active_r.push_back(i);
 		}
 	}
 	cilk_sync;
-	return active;
-//	return active.get_value();
+	active = active_r.get_value();
+	return !active.empty();
 }
 
 //stage 1
-void parallel_solver::push(int i)
+bool reducer_list::push(int i)
 {
 	vertex *v = vertices->at(i);
 	int excess = v->excess();
 	if (excess <= 0)
-		return;
+		return false;
 	vector<edge*>* edges = v->edges();
 	int h = v->height();
 	cilk_for (int j = 0; j < edges->size(); ++j) {
 		edge *e = edges->at(j);
-		vertex *op = e->opposite(v);
 		int res = e->residue(v);
-		if (h == op->height()+1 && res > 0)
+		if (h == e->opposite(v)->height()+1 &&
+			res > 0)
 		{
 			bool updated = false;
 			int d;
@@ -143,7 +124,6 @@ void parallel_solver::push(int i)
 			if (updated) {
 				e->update_flow(v, d);
 				int k = e->opposite(v)->index();
-				excess_tmp[k] += d;
 				pthread_mutex_lock(&vlocks[k]);
 				ex[k] += d;
 				pthread_mutex_unlock(&vlocks[k]);
@@ -152,9 +132,12 @@ void parallel_solver::push(int i)
 	}
 	cilk_sync;
 	v->update_excess(excess - v->excess());
+	if (excess > 0)
+		return true;
+	return false;
 }
 
-void parallel_solver::relabel(int i)
+void reducer_list::relabel(int i)
 {
 	assert(i > 1);
 	vertex* v = vertices->at(i);
